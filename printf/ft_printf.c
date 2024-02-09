9481ae8ba6cb334cf79b06df5b77076ed66942e7
Gabriel Nicoliello <gnicolie@c3r9s3.42malaga.com>
2024-02-09 15:33:45 +0100

    printfmain

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -6,16 +6,56 @@
 /*   By: gnicolie <gnicolie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 19:50:47 by gnicolie          #+#    #+#             */
-/*   Updated: 2024/02/08 20:02:35 by gnicolie         ###   ########.fr       */
+/*   Updated: 2024/02/09 15:31:55 by gnicolie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_printf(char const *str, ...)
+static int	ft_format(char c, va_list args)
 {
-	va_list	args;
-	int		i;
+	int	i;
 
-	
+	if (c == 'c')
+		i += ft_pchar(va_arg(args, char));//char
+	else if (c == 's')
+		i += ft_pchar(va_arg(args, int));
+	else if (c == 'p')
+		i += ft_pchar(va_arg(args, int));
+	else if (c == 'd')
+		i += ft_pchar(va_arg(args, int));
+	else if (c == 'i')
+		i += ft_pchar(va_arg(args, int));
+	else if (c == 'u')
+		i += ft_pchar(va_arg(args, int));
+	else if (c == 'x')
+		i += ft_pchar(va_arg(args, int));
+	else if (c == 'X')
+		i += ft_pchar(va_arg(args, int));
+	else if (c == '%')
+		i += ft_pchar(va_arg(args, int));
+	return (i);
+}
+
+int	ft_printf(char const *format, ...)
+{
+	int		i;
+	va_list	args;
+
+	va_start(args, format);
+	i = 0;
+	while (*format == '\0')
+	{
+		if (format == '%')
+		{
+			i += ft_format(++format, args);
+		}
+		else
+		{
+			write (1, format, 1);
+			i++;
+		}
+	}
+	va_end(args);
+	return (i);
 }
--- printf/ft_printf.h
+++ printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: gnicolie <gnicolie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/27 21:44:57 by gnicolie          #+#    #+#             */
-/*   Updated: 2024/02/08 19:55:30 by gnicolie         ###   ########.fr       */
+/*   Updated: 2024/02/09 14:15:03 by gnicolie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,6 @@
 # include "ft_printf.h"
 # include "./libft/libft.h"
 
-int	ft_printf(char const *str, ...);
+int	ft_printf(char const *format, ...);
 
 #endif
