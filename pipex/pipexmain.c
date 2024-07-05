b4478339dd888b979a637054f64c4b9869f06360
Gabriel Nicoliello <gnicolie@c3r3s4.42malaga.com>
2024-07-05 22:05:02 +0200

    pipex

--- pipex/pipexmain.c
+++ pipex/pipexmain.c
@@ -6,20 +6,57 @@
 /*   By: gnicolie <gnicolie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/02 17:13:07 by gnicolie          #+#    #+#             */
-/*   Updated: 2024/06/30 21:13:04 by gnicolie         ###   ########.fr       */
+/*   Updated: 2024/07/05 22:00:26 by gnicolie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-char	**findenv(char **envp)
+char	**findpaths(char **envp)
 {
-	
+	char	**paths;
+
+	while (ft_strncmp(*envp, "PATH", 4) != 0)
+		envp++;
+	paths = ft_split(*envp +5, ':');
+	return (paths);
 }
+
+char	**getargs(char **argv, int argc)
+{
+	char	**args;
+
+	*argv = *argv +2;
+	while (ft_strncmp(*envp, "PATH", 4) != 0)
+		envp++;
+	paths = ft_split(*envp +5, ' ');
+	return (paths);
+}
+
+/* char	execfunc(char *path, char *cmd )
+{
+	while (acc)
+	{
+		
+	}
+	
+} */
 
 int	main(int argc, char **argv, char **envp)
 {
-	char	**path;
+	char	**paths;
+	char	**args;
 
-	path = findenv(envp);
+	if (argc != 5)
+		ft_printf("ERROR");
+	paths = findpaths(envp);
+	args = getargs(argv, argc);
+
+	/* ft_printf("%s\n",argv[0]);
+	while (*paths)
+	{
+		ft_printf("%s\n",*paths);
+		paths++;
+	} */
 }
+
